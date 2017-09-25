c2e0548ad9e30428026dfbe2caf76846a5a3f153
Fábio Akira Yoshida <fabioseno@gmail.com>
2017-09-25 10:29:58 -0300

    New model template

--- templates/nodejs/model.js
+++ templates/nodejs/model.js
@@ -1,61 +1,23 @@
 /*global require, module*/
 var mongoose = require('mongoose'),
     userSchema = new mongoose.Schema({
-        firstName: {
-            required: true,
-            type: String,
-            trim: true
-        },
-        lastName: {
-            required: true,
-            type: String,
-            trim: true
-        },
-        email: {
-            required: true,
-            type: String,
-            trim: true
-        },
-        birthday: {
-            required: true,
-            type: Date
-        },
-        login: {
-            required: true,
-            type: String
-        },
-        password: {
-            required: true,
-            type: String
-        },
-        creationDate: {
-            required: true,
-            type: Date
-        },
-        lastLogin: {
-            type: Date
-        },
-        status: {
-            required: true,
-            type: String
-        }
+        //{fields}
+        //model.creationDate = new Date();
     });
 
-userSchema.virtual('id').get(function () {
+    {entityModelSchema}.virtual('id').get(function () {
     'use strict';
 
     return this._id.toHexString();
 });
 
-userSchema.methods.toJSON = function () {
-    var obj = this.toObject()
-    delete obj.password
-    delete obj.creationDate
-    delete obj.lastLogin
-    delete obj.__v
-    return obj
-}
+// {entityModelSchema}.methods.toJSON = function () {
+//     var obj = this.toObject()
+//     delete obj.password
+//     delete obj.creationDate
+//     delete obj.lastLogin
+//     delete obj.__v
+//     return obj
+// }
 
-module.exports = mongoose.model('User', userSchema);
-
-model.creationDate = new Date();
+module.exports = mongoose.model('{entityName}', {entityModelSchema});
