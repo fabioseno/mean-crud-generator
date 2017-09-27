34472c46546a12edd77b957b96cfbcebab7fda75
Fábio Akira Yoshida <fabioseno@gmail.com>
2017-09-27 00:41:35 -0300

    Model template cleanup

--- templates/nodejs/model.js
+++ templates/nodejs/model.js
@@ -1,7 +1,6 @@
 /*global require, module*/
 var mongoose = require('mongoose'),
     userSchema = new mongoose.Schema({fields});
-    //model.creationDate = new Date();
 );
 
 {entityModelSchema}.virtual('id').get(function () {
@@ -9,14 +8,5 @@ var mongoose = require('mongoose'),
 
     return this._id.toHexString();
 });
-
-// {entityModelSchema}.methods.toJSON = function () {
-//     var obj = this.toObject()
-//     delete obj.password
-//     delete obj.creationDate
-//     delete obj.lastLogin
-//     delete obj.__v
-//     return obj
-// }
 
 module.exports = mongoose.model('{modelName}', {entityModelSchema});
