aeb5bc2398092fca6cc89fca9a1e7454ccd4eea0
Fábio Akira Yoshida <fabioseno@gmail.com>
2017-10-01 00:47:03 -0300

    Adding dynamic fields

--- generators/frontend-generator.js
+++ generators/frontend-generator.js
@@ -2,7 +2,7 @@ var tools = require('./tools');
 var os = require('os');
 var frontendFolder = 'angularjs';
 
-function capitalize (string) {
+function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
@@ -44,7 +44,7 @@ function generateListViewHtml(config, cb) {
     }
 
     template = template.replace(/{listing_fields}/g, listingFieldsOutput);
-    
+
     tools.writeFile('/pages/' + config.entityName + '-list.html', template);
 
     cb(null, true);
@@ -59,7 +59,7 @@ function generateListViewLogic(config, cb) {
     template = template.replace(/{model_name}/g, config.model.name);
     template = template.replace(/{plural_name}/g, config.model.pluralName);
     template = template.replace(/{model_plural_name}/g, capitalize(config.entityName) + 's');
-    
+
     tools.writeFile('/pages/' + config.entityName + '-list.js', template);
 
     cb(null, true);
@@ -73,6 +73,24 @@ function generateDetailsViewHtml(config, cb) {
     template = template.replace(/{details_view_page_title}/g, config.pages.detailsViewPageTitle);
     template = template.replace(/{model_name}/g, config.model.name);
 
+    var controls = '';
+    for (var i = 0; i < config.fields.length; i++) {
+        var field = config.fields[i];
+
+        var required = field.required ? 'required' : '';
+        var messages = required ? `<div ng-messages="vm.form.` + field.fieldName + `.$error" multiple ng-if="vm.form.$submitted || vm.form.` + field.fieldName + `.$dirty">
+            <div ng-message="required">Campo obrigatório</div>
+        </div>` : '';
+        
+        controls += `<div class="form-group col-lg-12">
+        <label for="` + field.fieldName + `">` + field.model.name + `</label>
+        <input type="text" id="` + field.fieldName + `" name="` + field.fieldName + `" ` + required + ` class="form-control" data-ng-model="vm.` + config.entityName + `.` + field.fieldName + `">
+        ` + messages + `
+    </div>`;
+    }
+
+    template = template.replace(/{controls}/g, controls);
+
     tools.writeFile('/pages/' + config.entityName + '-details.html', template);
 
     cb(null, true);
@@ -82,20 +100,16 @@ function generateDetailsViewLogic(config, cb) {
     console.log('Generating details view logic...');
 
     var template = tools.readTemplate(frontendFolder, 'details.js');
-    
+
     template = template.replace(/{entity_name}/g, config.entityName);
     template = template.replace(/{plural_name}/g, config.model.pluralName);
     template = template.replace(/{entity_title}/g, config.entityTitle);
     template = template.replace(/{entity_plural_title}/g, config.entityPluralTitle);
-    
+
     template = template.replace(/{details_view_page_title}/g, config.pages.detailsViewPageTitle);
 
-    var controls;
-
-    template = template.replace(/{controls}/g, controls);
-    
     tools.writeFile('/pages/' + config.entityName + '-details.js', template);
-    
+
     cb(null, true);
 }
 
