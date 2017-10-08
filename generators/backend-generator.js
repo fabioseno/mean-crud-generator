f7787bb88b104f1f788a911f13c654f137b28517
Fábio Akira Yoshida <fabioseno@gmail.com>
2017-10-07 21:34:14 -0300

    Middleware generator refactoring

--- generators/backend-generator.js
+++ generators/backend-generator.js
@@ -61,54 +61,8 @@ function generateMiddlewares(config, cb) {
     var template = tools.readTemplate(backendFolder, 'middleware.js');
 
     template = template.replace(/{entity_name}/g, config.entityName);
-
-    // required
-    var required = '';
-
-    required += 'module.exports.required = function (req, res, next) {;' + os.EOL;
-    required += '\t\'use strict\';' + os.EOL + os.EOL;
-
-    required += '\treq.validations = req.validations || [];' + os.EOL + os.EOL;
-
-    for (var i = 0; i < config.fields.length; i++) {
-        var field = config.fields[i];
-
-        if (field.required) {
-            required += '\tif (!req.body || !req.body.' + field.fieldName + ') {' + os.EOL;
-            required += '\t\treq.validations.push(\'Campo ' + field.fieldLabel.toLowerCase() + ' é obrigatório!\');' + os.EOL;
-            required += '\t}' + os.EOL + os.EOL;
-        }
-    }
-
-    required += '\tnext();' + os.EOL;
-    required += '};' + os.EOL + os.EOL;
-
-    template = template.replace(/{field_required}/g, required);
-
-    // unique
-    var unique = '';
-
-    for (i = 0; i < config.fields.length; i++) {
-        var field = config.fields[i];
-
-        if (field.unique) {
-            unique += `module.exports.` + field.fieldName + `Exists = function (req, res, next) {;` + os.EOL
-            unique += '\t\'use strict\';' + os.EOL + os.EOL;
-
-            unique += '\t' + config.model.name + '.findOne({' + field.fieldName + ': ' + 'req.query.' + field.fieldName + '}, function (err, result) {' + os.EOL;
-            unique += '\t\tif (result && req.query.' + field.fieldName + ' & result.' + field.fieldName + ' != req.query.' + field.fieldName + ') {' + os.EOL;
-
-            unique += '\t\t\treq.validations = req.validations || [];' + os.EOL;
-            unique += '\t\t\treq.validations.push(\'' + capitalize(config.entityTitle) + ' com ' + field.fieldLabel.toLowerCase() + ' já cadastrado!\');' + os.EOL
-            unique += '\t\t}' + os.EOL + os.EOL;
-
-            unique += '\t\tnext();' + os.EOL;
-            unique += '\t});' + os.EOL;
-            unique += '};' + os.EOL + os.EOL;
-        }
-    }
-
-    template = template.replace(/{field_exists}/g, unique);
+    template = template.replace(/{field_required}/g, util.getMiddlewareRequiredFunctions(config));
+    template = template.replace(/{field_exists}/g, util.getMiddlewareUniqueFunctions(config));
 
     tools.writeFile('/middlewares/' + config.entityName + '.js', template);
 
--- generators/util.js
+++ generators/util.js
@@ -107,13 +107,66 @@ function getRouteUniqueMiddleware(config) {
     return middlewareList;
 }
 
+function getMiddlewareRequiredFunctions(config) {
+    var required = '';
+
+    required += 'module.exports.required = function (req, res, next) {;' + os.EOL;
+    required += '\t\'use strict\';' + os.EOL + os.EOL;
+
+    required += '\treq.validations = req.validations || [];' + os.EOL + os.EOL;
+
+    for (var i = 0; i < config.fields.length; i++) {
+        var field = config.fields[i];
+
+        if (field.required) {
+            required += '\tif (!req.body || !req.body.' + field.fieldName + ') {' + os.EOL;
+            required += '\t\treq.validations.push(\'Campo ' + field.fieldLabel.toLowerCase() + ' é obrigatório!\');' + os.EOL;
+            required += '\t}' + os.EOL + os.EOL;
+        }
+    }
+
+    required += '\tnext();' + os.EOL;
+    required += '};' + os.EOL + os.EOL;
+
+    return required;
+}
+
+function getMiddlewareUniqueFunctions(config) {
+    var unique = '';
+
+    for (i = 0; i < config.fields.length; i++) {
+        var field = config.fields[i];
+
+        if (field.unique) {
+            unique += `module.exports.` + field.fieldName + `Exists = function (req, res, next) {;` + os.EOL
+            unique += '\t\'use strict\';' + os.EOL + os.EOL;
+
+            unique += '\t' + config.model.name + '.findOne({' + field.fieldName + ': ' + 'req.query.' + field.fieldName + '}, function (err, result) {' + os.EOL;
+            unique += '\t\tif (result && req.query.' + field.fieldName + ' & result.' + field.fieldName + ' != req.query.' + field.fieldName + ') {' + os.EOL;
+
+            unique += '\t\t\treq.validations = req.validations || [];' + os.EOL;
+            unique += '\t\t\treq.validations.push(\'' + capitalize(config.entityTitle) + ' com ' + field.fieldLabel.toLowerCase() + ' já cadastrado!\');' + os.EOL
+            unique += '\t\t}' + os.EOL + os.EOL;
+
+            unique += '\t\tnext();' + os.EOL;
+            unique += '\t});' + os.EOL;
+            unique += '};' + os.EOL + os.EOL;
+        }
+    }
+
+    return unique;
+}
+
 module.exports = {
     getModelMetadata: getModelMetadata,
 
     getControllerSearchCriteria: getControllerSearchCriteria,
     getControllerUpdateFields: getControllerUpdateFields,
-    
+
     getRouteValidationDeclaration: getRouteValidationDeclaration,
     getRouteRequiredMiddleware: getRouteRequiredMiddleware,
-    getRouteUniqueMiddleware: getRouteUniqueMiddleware
+    getRouteUniqueMiddleware: getRouteUniqueMiddleware,
+
+    getMiddlewareRequiredFunctions: getMiddlewareRequiredFunctions,
+    getMiddlewareUniqueFunctions: getMiddlewareUniqueFunctions
 };
