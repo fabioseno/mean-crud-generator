814f705ed31796998510dd9a7eb94915d76d2c18
fabioseno <fabio@aixmobil.com>
2020-01-28 22:57:11 -0300

    Fix

--- generators/mean-2.0/templates/backend/nodejs/route.js
+++ generators/mean-2.0/templates/backend/nodejs/route.js
@@ -11,10 +11,10 @@ module.exports = function (context) {
     router.get('/{entity_plural_name}/:id', auth.isLogged, {controller_name}.get);
     
     // Add
-    router.put('/{entity_plural_name}', auth.isLogged, {required_middleware}{unique_middleware}{controller_name}.add);
+    router.post('/{entity_plural_name}', auth.isLogged, {required_middleware}{unique_middleware}{controller_name}.add);
     
     // Update
-    router.post('/{entity_plural_name}/:id', auth.isLogged, {required_middleware}{unique_middleware}{controller_name}.update);
+    router.put('/{entity_plural_name}/:id', auth.isLogged, {required_middleware}{unique_middleware}{controller_name}.update);
     
     // Remove
     router['delete']('/{entity_plural_name}/:id', auth.isLogged, {controller_name}.remove);
